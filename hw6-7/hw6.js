7e3240f5a468558570e3af69491f66e5090d7389
taratkevich.ann <taratkevich.ann@ukr.net>
2021-02-04 23:24:58 +0200

    finished the 6th homework

--- hw6-7/hw6.js
+++ hw6-7/hw6.js
@@ -1,14 +1,14 @@
-function Figure(x, y, color){
+function Figure(x, y, color) {
     this.x = x
     this.y = y
     this.color = color
 }
 
-function Line (x, y, x2, y2 , color){
+function Line(x, y, x2, y2, color) {
     Figure.call(this, x, y, color)
     this.secondX = x2;
     this.secondY = y2;
-    this.draw = function(ctx){
+    this.draw = function (ctx) {
         ctx.beginPath()
         ctx.moveTo(x2, y2)
         ctx.lineTo(x, y)
@@ -17,11 +17,11 @@ function Line (x, y, x2, y2 , color){
     }
 }
 
-function Rect (x, y, w, h ,color){
+function Rect(x, y, w, h, color) {
     Figure.call(this, x, y, color)
     this.width = w
     this.height = h
-    this.draw = function(ctx){
+    this.draw = function (ctx) {
         ctx.beginPath()
         ctx.rect(x, y, w, h)
         ctx.fillStyle = color
@@ -29,29 +29,29 @@ function Rect (x, y, w, h ,color){
     }
 }
 
-function Circle (x, y, r, color){
+function Circle(x, y, r, color) {
     Figure.call(this, x, y, color)
     this.radius = r;
-    this.draw = function(ctx){
+    this.draw = function (ctx) {
         ctx.beginPath()
-        ctx.arc(x, y, r, 0, 2*Math.PI)
+        ctx.arc(x, y, r, 0, 2 * Math.PI)
         ctx.fillStyle = color
         ctx.fill()
     }
 }
 
-function Canvas(canvasId){
+function Canvas(canvasId) {
     this.canvas = document.getElementById(canvasId)
     this.ctx = this.canvas.getContext('2d')
-    this.add = function (...figures){
-        for(let i = 0; i < figures.length; i++){
+    this.add = function (...figures) {
+        for (let i = 0; i < figures.length; i++) {
             figures[i].draw(this.ctx)
         }
     }
 }
 
-let line = new Line( 50, 250, 200, 200, 'rgb(121, 59, 209)');
-let line2 = new Line( 60, 260, 210, 210, 'rgb(121, 59, 209)');
+let line = new Line(50, 250, 200, 200, 'rgb(121, 59, 209)');
+let line2 = new Line(60, 260, 210, 210, 'rgb(121, 59, 209)');
 let rect = new Rect(260, 170, 60, 120, 'rgb(241, 245, 118)')
 let rect2 = new Rect(280, 150, 120, 45, 'rgba(30, 194, 199, .8)')
 let rect3 = new Rect(360, 180, 60, 50, 'rgb(145, 245, 118)')
@@ -60,11 +60,11 @@ let smallCircle = new Circle(30, 30, 30, 'rgba(255, 102, 153, .7)')
 
 let drawArea = new Canvas('canvas');
 
-for(let i = 0; i <= drawArea.canvas.offsetWidth; i++){
-    if (i%2){
-        drawArea.add(new Line(i*10+10, 0 , i*10, 10, 'blue'))
-    } else{
-        drawArea.add(new Line(i*10, 0, i*10+10, 10, 'blue'))
+for (let i = 0; i <= drawArea.canvas.offsetWidth; i++) {
+    if (i % 2) {
+        drawArea.add(new Line(i * 10 + 10, 0, i * 10, 10, 'blue'))
+    } else {
+        drawArea.add(new Line(i * 10, 0, i * 10 + 10, 10, 'blue'))
     }
 }
 
